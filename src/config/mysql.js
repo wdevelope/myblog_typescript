3cee144d0eef01a60119331e464d04338689fa5d
seongwong woo <dnqkr18@gmail.com>
2023-12-06 09:00:47 +0900

    chore: mysql ec2서버 내 주소로 변경

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 module.exports = sequelize;
