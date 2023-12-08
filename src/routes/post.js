c9916ccaa2bef2dc4fd5b62025ddbdc8857b9734
seongwong woo <dnqkr18@gmail.com>
2023-12-08 17:48:39 +0900

    chore: 빌드수정 및 게시글 수정 router 미들웨어  수정

--- src/routes/post.js
+++ src/routes/post.js
@@ -9,7 +9,7 @@ router.post('/', auth, validation.createPost, postController.create);
 // 특정 카테고리 게시글 전체조회
 router.get('/', postController.getAllpost);
 // 게시글 수정
-router.patch('/:id', auth, validation.createPost, postController.patch);
+router.patch('/:id', auth, postController.patch);
 // 게시글 삭제
 router.delete('/:id', auth, postController.delete);
 // 게시글 상세조회
