e73ac9b5c78e0cfbf6d8c545671c4bb22d0e78b1
seongwong woo <dnqkr18@gmail.com>
2023-12-09 22:29:19 +0900

    chore: 유저 인증정보 req.user에서 res로 변경

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,38 +1,38 @@
 import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 
-interface ExtendedRequest extends Request {
-  user?: any;
+interface UserPayload {
+  userId: number;
+  status: string;
 }
 
 // 유저 인증 체크
-function auth(req: ExtendedRequest, res: Response, next: NextFunction): void {
+const auth = (req: Request, res: Response, next: NextFunction) => {
   const BearerToken = req.cookies.Authorization;
-
   if (!BearerToken) {
     res.status(401).json({ errorMessage: '토큰이 공급되지 않았습니다.' });
     return;
   }
 
   const token = BearerToken.split(' ')[1];
-
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-    req.user = decoded as any;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as UserPayload;
+    res.locals.user = decoded;
     next();
   } catch (ex) {
     console.error('토큰 검증 오류:', ex);
     res.status(400).json({ errorMessage: '토큰 인증 실패' });
   }
-}
+};
 
 // 관리자 권한 체크
-function authAdmin(req: ExtendedRequest, res: Response, next: NextFunction): void {
-  if (req.user && req.user.status !== 'admin') {
+const authAdmin = (req: Request, res: Response, next: NextFunction) => {
+  const user = (req as any).user as UserPayload;
+  if (user && user.status !== 'admin') {
     res.status(403).json({ errorMessage: '관리자 권한이 필요합니다.' });
     return;
   }
   next();
-}
+};
 
 export { auth, authAdmin };
