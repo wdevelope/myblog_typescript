1aa7b74b62d908b9f2d0cbf256af143ce9e94b20
seongwong woo <dnqkr18@gmail.com>
2023-12-16 16:47:44 +0900

    chore: 정적경로  수정

--- src/app.ts
+++ src/app.ts
@@ -25,12 +25,12 @@ app.use(
 // API 라우트 설정
 app.use(router);
 
-// 프론트 정적 파일 제공
-app.use(express.static('public'));
+// 프론트 정적 파일 경로
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Vue Router를 위한 와일드카드 라우트
-app.get('*', (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '/dist/public/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.use(router);
